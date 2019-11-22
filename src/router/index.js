f73f630fa98c88bd01ebb04f8399b42fc6b268e6
wey <149201216@qq.com>
2019-11-22 11:20:22 +0800

    配置各个模块的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/user",
-    component: { render: h => h("router-view") },
+    component: () =>
+      import(/* webpackChunkName: "layout" */ "../layouts/UserLayout"),
     children: [
+      {
+        path: "/user",
+        redirect: "/user/login" //重定向
+      },
       {
         path: "/user/login",
         name: "login",
@@ -25,8 +29,81 @@ const routes = [
   },
   {
     path: "/",
-    name: "home",
-    component: Home
+    component: () =>
+      import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout"),
+    children: [
+      //dashboard
+      {
+        path: "/",
+        redirect: "/dashboard/analysis"
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/dashboard/analysis",
+            name: "analysis",
+            component: () =>
+              import(
+                /* webpackChunkName: "dashboard" */ "../views/Dashboard/Analysis"
+              )
+          }
+        ]
+      },
+
+      //form
+      {
+        path: "/form",
+        name: "form",
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/form/basic-form",
+            name: "basicform",
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/BasicForm")
+          },
+          {
+            path: "/form/step-form",
+            name: "stepform",
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/StepForm"),
+            children: [
+              {
+                path: "/form/step-form",
+                redirect: "/form/step-form/info"
+              },
+              {
+                path: "/form/step-form/info",
+                name: "info",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step1"
+                  )
+              },
+              {
+                path: "/form/step-form/comfirm",
+                name: "comfirm",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step2"
+                  )
+              },
+              {
+                path: "/form/step-form/result",
+                name: "result",
+                component: () =>
+                  import(
+                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step3"
+                  )
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
   {
     path: "/about",
