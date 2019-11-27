74e93987ce6b01177c0e04d19f4e20e5e7c961fa
wey <149201216@qq.com>
2019-11-27 19:43:28 +0800

    完善chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartDom" style="height: 400px;"></div>
+  <div ref="chartDom"></div>
 </template>
 
 <script>
@@ -8,40 +8,42 @@ import debounce from "lodash/debounce";
 import { addListener, removeListener } from "resize-detector";
 
 export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    option(val) {
+      this.chart.setOption(val);
+    }
+    // option: {
+    //   handler(val) {
+    //     this.chart.setOption(val);
+    //   },
+    //   deep: true
+    // }
+  },
   created() {
     this.resize = debounce(this.resize, 300);
   },
   mounted() {
-    // 基于准备好的dom，初始化echarts实例
-    this.chart = echarts.init(this.$refs.chartDom);
+    this.renderChart();
     addListener(this.$refs.chartDom, this.resize);
-    // 绘制图表
-    this.chart.setOption({
-      title: {
-        text: "ECharts 入门示例"
-      },
-      tooltip: {},
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
-    });
   },
   beforeDestroy() {
     removeListener(this.$refs.chartDom, this.resize);
     this.chart.dispose();
+    this.chart = null;
   },
   methods: {
     resize() {
-      console.log("resize");
       this.chart.resize();
+    },
+    renderChart() {
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption(this.option);
     }
   }
 };
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,11 +1,46 @@
 <template>
-  <div><Chart /></div>
+  <div>
+    <Chart :option="chartOption" style="height: 400px" />
+  </div>
 </template>
 
 <script>
+import random from "lodash/random";
 import Chart from "../../components/Chart";
 
 export default {
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "各类服饰销量"
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.interal = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+      );
+      this.chartOption = { ...this.chartOption };
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interal);
+  },
   components: {
     Chart
   }
